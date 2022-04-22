d062b106bc4a6ebc2e1964ab9612a5ab109f21a4
SergeitZ <sergiofranco6@gmail.com>
2022-04-21 21:19:02 -0400

    Initial commit

---
+++ src/main/java/com/dataset/bitcoindataset/BitcoindatasetApplication.java
@@ -0,0 +1,13 @@
+package com.dataset.bitcoindataset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitcoindatasetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitcoindatasetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dataset/bitcoindataset/BitcoindatasetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dataset.bitcoindataset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitcoindatasetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
